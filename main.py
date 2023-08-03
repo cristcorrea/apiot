05c93d06250cb91b39190f67530207172a20f524
cristcorrea <63624498+cristcorrea@users.noreply.github.com>
2023-08-03 19:43:38 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+
+
+from machine import Pin
+import dht
+import time
+import json
+from collections import OrderedDict
+
+sw = Pin(23, Pin.IN)
+led = Pin(2, Pin.OUT)
+d = dht.DHT22(Pin(25))
+
+
+while True:
+
+    try:
+        d.measure()
+        temperatura=d.temperature()
+        humedad=d.humidity()
+        datos=json.dumps(OrderedDict([
+            ('temperatura',temperatura),
+            ('humedad',humedad)
+        ]))
+        print(datos)
+    except OSError as e:
+        print("sin sensor")
+
+    time.sleep(20)
